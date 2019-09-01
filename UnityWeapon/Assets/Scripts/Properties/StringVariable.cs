518b5d8d07eff3f2736e1191b9d187e0cc7b7f3a
Dima <seaqqullprogramming@gmail.com>
2019-09-01 22:29:18 +0300

    Update: String variable
     Description: Used to connect different classes with some string value.

---
+++ UnityWeapon/Assets/Scripts/Properties/StringVariable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variable/String")]
+public class StringVariable : ScriptableObject
+{
+
+#if UNITY_EDITOR
+#pragma warning disable 0414
+    [Multiline]
+    [SerializeField] private string _description = "";
+#pragma warning restore 0414
+#endif
+    [SerializeField] private string _value;
+
+
+    public string Value
+    {
+        get { return this._value; }
+        private set { this._value = value; }
+    }
+
+
+    public void SetValue(string value)
+    {
+        Value = value;
+    }
+
+    public void SetValue(StringVariable value)
+    {
+        Value = value.Value;
+    }
+}
