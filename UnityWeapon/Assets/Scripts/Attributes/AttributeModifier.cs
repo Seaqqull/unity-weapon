25e3ecf840033a346b254133d9ab9557d8a40b33
Dima <seaqqullprogramming@gmail.com>
2019-09-01 22:02:46 +0300

    Update: Attribute modifier
     Description: Can be used to dynamically create/change attribute value.

---
+++ UnityWeapon/Assets/Scripts/Attributes/AttributeModifier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "AttributeModifier/Create")]
+public class AttributeModifier : ScriptableObject
+{
+    public enum ValueType { Relative, Absolute }
+
+    public string Name;
+    public ValueType Type;
+    [Range(0, ushort.MaxValue)] public float Value = 1.0f;
+}
