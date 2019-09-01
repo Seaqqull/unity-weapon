5fd90730a13ac0ff2e55cc5da4e360347a4dabb9
Dima <seaqqullprogramming@gmail.com>
2019-09-01 22:43:34 +0300

    Update: Attribute modifier
     Added:
     - Attack reference variable
     - Damage modifier (linked with Attack reference variable)
     Changes:
     - Attribute modifier can now be linked by attribute name

--- UnityWeapon/Assets/Scripts/Attributes/AttributeModifier.cs
+++ UnityWeapon/Assets/Scripts/Attributes/AttributeModifier.cs
@@ -5,7 +5,7 @@ public class AttributeModifier : ScriptableObject
 {
     public enum ValueType { Relative, Absolute }
 
-    public string Name;
+    public StringReference Name;
     public ValueType Type;
     [Range(0, ushort.MaxValue)] public float Value = 1.0f;
 }
