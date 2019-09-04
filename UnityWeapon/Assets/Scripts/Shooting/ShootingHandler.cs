1910ffc24cdc70d1dc259541664f9f7b7bc7c92f
Dima <seaqqullprogramming@gmail.com>
2019-09-04 22:48:35 +0300

    Update: Shooting
     Added:
     - Shooting handler
     - Shooting mode
     - Attack attribute
    
     Description:
     - Shooting handler: checks possibility of weapon shooting
     - Shooting mode: checks `ammoData` through `AmmoHandler`, performs weapon shooting

---
+++ UnityWeapon/Assets/Scripts/Shooting/ShootingHandler.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Computing/Shooting/Handler")]
+public class ShootingHandler : Utility.ComputingHandler
+{
+    // Some weapon methods, which control the possibility of a shot
+}
---
+++ UnityWeapon/Assets/Scripts/Shooting/ShootingMode.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Computing/Shooting/Mode")]
+public abstract class ShootingMode : ScriptableObject
+{
+    [SerializeField] protected ushort _bulletsToPerformShot = 1;
+    [SerializeField] protected AttributeModifier[] _attributes;
+
+    public abstract bool IsExecutable(Weapon weapon);
+    public abstract void Perform(Weapon weapon);
+}
